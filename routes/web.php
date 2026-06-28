afc853e66bceb3982c9882bd1517223458c15383
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-06-28 15:18:51 +0200

    basic auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PhotoController::class, 'index'])
-    ->middleware(\App\Http\Middleware\RestrictToSubnet::class)
-    ->name('camera');
+Route::middleware('auth.basic')->group(function () {
+    Route::get('/', [PhotoController::class, 'index'])->name('camera');
+});
